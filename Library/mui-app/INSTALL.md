3df816430d09da93dd78b8b2a84bc2f8c565f1a9
wwwalohacampus <h850415@naver.com>
2025-01-02 18:36:13 +0900

    🎨material design icon 테마

---
+++ Library/mui-app/INSTALL.md
@@ -0,0 +1,2 @@
+## Material Design Icons
+npm install @mui/icons-material @mui/material @emotion/react @emotion/styled
---
+++ Library/mui-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+function App() {
+
+  return (
+    <div>
+      <HomeIcon />
+      <SearchIcon />
+      <FavoriteIcon />
+    </div>
+  )
+}
+
+export default App
